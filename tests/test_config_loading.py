5ef47d46afaf6dcaa6c2647b2d7d20ae747f6f03
Grebennikov Roman <grv@dfdx.me>
2024-01-03 12:30:06 +0100

    fix test_load_msmarco

--- tests/test_config_loading.py
+++ tests/test_config_loading.py
@@ -4,5 +4,5 @@ from transformers import HfArgumentParser
 
 def test_load_msmarco():
     parser = HfArgumentParser((ModelArguments, DatasetArguments, BiencoderTrainingArguments))
-    model_args, dataset_args, training_args = parser.parse_json_file(json_file="examples/msmarco.json")
+    model_args, dataset_args, training_args = parser.parse_json_file(json_file="tests/config/msmarco.json")
     assert model_args.model_name_or_path == "sentence-transformers/all-MiniLM-L6-v2"
