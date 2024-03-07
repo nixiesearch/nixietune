7b23f6fa01a5cefa8077b5a791696d41c4bd8795
Grebennikov Roman <grv@dfdx.me>
2024-03-07 16:39:33 +0100

    ci: fix test_config_loading

--- tests/biencoder/test_config_loading.py
+++ tests/biencoder/test_config_loading.py
@@ -4,5 +4,7 @@ from transformers import HfArgumentParser
 
 def test_load_esci():
     parser = HfArgumentParser((ModelArguments, DatasetArguments, BiencoderTrainingArguments))
-    model_args, dataset_args, training_args = parser.parse_json_file(json_file="examples/esci.json")
+    model_args, dataset_args, training_args = parser.parse_json_file(
+        json_file="tests/biencoder/config/esci_infonce.json"
+    )
     assert model_args.model_name_or_path == "sentence-transformers/all-MiniLM-L6-v2"
